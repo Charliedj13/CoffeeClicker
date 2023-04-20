7ca65fd9ab92c358a525556897d37a7b3e3ee5fe
Charlie Jenkins <charliedj13@gmail.com>
2023-04-20 13:09:47 -0500

    final revisions

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ let keurigButton = document.getElementById("keurig-maker")
 let coffeeShop = document.getElementById("shop")
 let coffeeShopButton = document.getElementById("coffeeshop");
 let frenchButton = document.getElementById("french-button");
-// let intervalTimerId = undefined;
-// let intervalTimerId2 = undefined;
-// let intervalTimerId3 = undefined;
 
 function myButtonsCallbackFunction () {
     console.log("The button was pressed")
@@ -26,6 +23,7 @@ let numOfFrench = 0;
 let numOfKeurig = 0;
 let numOfCoffeeShop = 0;
 let gameStarted = false
+
 function changeTheCoffeeCounter () {
     console.log("Number was just added")
     clickerCount = clickerCount + 1;
@@ -36,11 +34,12 @@ function buyFrench () {
     if (clickerCount >= 50) {
         clickerCount = clickerCount - 50
         coffeePerSecond += 1
+        coffeee.textContent = coffeePerSecond
     } else if (clickerCount < 50) {
-        console.log("Not enough coffee")
         alert("Not Enough Cofffee")
     }
 }
+frenchButton.addEventListener("click", buyFrench)
 
 window.addEventListener("click", function (){
     console.log(gameStarted)
@@ -51,48 +50,28 @@ window.addEventListener("click", function (){
 })
 // When you try to buy anything, you want to check to see if I have enough coffee, if I do, subtract the cost of the producer from the coffee count. Then take however much per second that producer gives you and add that value to that coffee per second.
 function increaseCountPerSecond () {
-    // let sumOfCoffee = 0
-    // sumOfCoffee += KeurigPerSecond
-    // sumOfCoffee += frenchPerSecond
-    // sumOfCoffee += coffeeShopPerSecond
-    // coffeePerSecond += sumOfCoffee
-    console.log(coffeePerSecond)
     clickerCount = clickerCount + coffeePerSecond
     coffeeCount.textContent = clickerCount;
 }
-frenchButton.addEventListener("click", buyFrench)
 
 function buyKeurig (){
     if (clickerCount >= 500) {
         clickerCount = clickerCount - 500
         coffeePerSecond += 10
+        coffeee.textContent = coffeePerSecond
     } else if (clickerCount < 500) {
-        console.log("Not Enough Coffee")
         alert("Not enough Coffee")
     }
 }
-// function setIntervalKeurig () {
-//     intervalTimerId = setInterval(changeTheCoffeeCounter, 200)
-//     clickerCount + intervalTimerId
-//     console.log(clickerCount - 500);
-//     console.log(coffeeSecond + 10);
 
 keurigButton.addEventListener("click", buyKeurig)
-
-// if (clickerCount >= 50 && keurigButton.addEventListener("click", setIntervalKeurig)){
-//     setIntervalKeurig ()
-// } else if (clickerCount < 50 && keurigButton.addEventListener("click", setIntervalKeurig)){
-// console.log("Not Enough Coffee")
-// alert("Not Enough Coffee")
-
-
 
 function buyCoffeeShop (){
     if (clickerCount >= 5000) {
         clickerCount = clickerCount - 5000
         coffeePerSecond += 25 
+        coffeee.textContent = coffeePerSecond
     } else if (clickerCount < 5000) {
-        console.log("Not Enough Coffee")
         alert("Not enough Coffee")
     }
 }
